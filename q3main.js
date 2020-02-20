090ee19be12bdbee45776302215e09f56ad30f1a
vermav27 <vermav27@users.noreply.github.com>
2020-02-20 18:33:46 +0530

    Add files via upload

---
+++ q3main.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+//var ob = {a:1,b:2,c:3,d:4,e:5};
+//for (var key in ob) console.log(ob[key]);
+
+var ob = {a:1,b:2,c:3,d:4,e:5};
+var sum = ob.a + ob.b + ob.c + ob.d + ob.e;
+ob.f = sum;
+
+for (var key in ob) console.log(ob[key]);
+
+//var m = ob[]toString();
